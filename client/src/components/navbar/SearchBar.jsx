f9be116bebfd9ec5ea028d1a7b125f95f9d8a9ee
bbroscheit <bernardo.broscheit@gmail.com>
2022-09-29 21:38:05 -0300

    search bar

--- client/src/components/navbar/SearchBar.jsx
+++ client/src/components/navbar/SearchBar.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
-import { getGamesByName } from '../../actions/index';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { getGamesByName } from "../../actions/index";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
@@ -9,23 +9,41 @@ export default function SearchBar() {
   const dispatch = useDispatch();
   const [input, setInput] = useState("");
 
-  function handleInput(e){
-    e.preventDefault()
-    setInput(e.target.value)
+  function handleInput(e) {
+    e.preventDefault();
+    setInput(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(getGamesByName(input));
     
   }
 
-  function handleSubmit(e){
-    e.preventDefault()
-    dispatch(getGamesByName(input))
-     
+  function handleKeyDown(e){
+    e.preventDefault();
+    dispatch(getGamesByName(input));
   }
-  
+
   return (
-    <div className='searchBarContainer'>
-      <input className="inputSearchBar" type="text" placeholder='Search ....' onChange={e => handleInput(e)}/>  
-      <button className="buttonSearchBar" type='submit' onClick={e => handleSubmit(e)} ><FaSearch /></button> 
+    <div className="searchBarContainer">
+      <input
+        className="inputSearchBar"
+        type="text"
+        placeholder="Search ...."
+        onChange={(e) => handleInput(e)}
+        onKeyDown={(e) => {if(e.key === "Enter"){handleKeyDown(e)}}}
+      />
+      <button
+        className="buttonSearchBar"
+        type="submit"
+        onClick={(e) => handleSubmit(e)}
+        
+        
+      >
+        <FaSearch />
+      </button>
       {/* onKeyDown={(e) =>{if(e.key === 'Enter'){handleSubmit()}}}  */}
     </div>
-  )
+  );
 }
