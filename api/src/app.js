ee5c51d02d5f9c29ade43fba4f515bc972114599
bbroscheit <bernardo.broscheit@gmail.com>
2023-05-03 15:26:17 -0300

    agregado de cors

--- api/src/app.js
+++ api/src/app.js
@@ -25,7 +25,7 @@ server.use((req, res, next) => {
 
 server.use('/', routes);
 server.use(express.json())
-//server.use(cors())
+server.use(cors())
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
